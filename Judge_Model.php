402881b2a6a79e511cb0d810318a66a8c1b698a5
emartinez48 <eduardomartinez@Eduardos-MacBook-Pro.local>
2016-10-17 09:19:13 -0600

    Model

---
+++ Judge_Model.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Judge_Model extends CI_Model
+{
+     function __construct()
+     {
+          // Call the Model constructor
+          parent::__construct();
+          $this->load->database();
+     }
+
+     //get the username & password from tbl_usrs
+     function get_user($usr, $pwd)
+     {
+          $sql = "select * from Judge where JudgeName = '" . $usr . "' and JudgePass = '" . $pwd . "' ''";
+          $query = $this->db->query($sql);
+          return $query->num_rows();
+     }
+     public function get_Posters($slug = FALSE)
+     {
+             if ($slug === FALSE)
+             {
+                     $query = $this->db->get('Poster');
+                     return $query->result_array();
+             }
+
+             $query = $this->db->get_where('Poster', array('slug' => $slug));
+             return $query->row_array();
+     }
+
+}?>
